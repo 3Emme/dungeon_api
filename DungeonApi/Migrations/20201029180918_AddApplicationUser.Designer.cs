5e5e238ca429aaba28c468e590974c4a23ccfd99
3Emme <emmettbuentiempo@gmail.com>
2020-10-29 11:15:51 -0700

    complete migration AddApplicationUser.
    Co-Authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- DungeonApi/Migrations/20201029180918_AddApplicationUser.Designer.cs
+++ DungeonApi/Migrations/20201029180918_AddApplicationUser.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DungeonApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DungeonApi.Migrations
 {
     [DbContext(typeof(DungeonApiContext))]
-    partial class DungeonApiContextModelSnapshot : ModelSnapshot
+    [Migration("20201029180918_AddApplicationUser")]
+    partial class AddApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DungeonApi/Migrations/20201029180918_AddApplicationUser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DungeonApi.Migrations
+{
+    public partial class AddApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Monsters",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    NormalizedUserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    NormalizedEmail = table.Column<string>(nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Monsters_UserId",
+                table: "Monsters",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Monsters_ApplicationUser_UserId",
+                table: "Monsters",
+                column: "UserId",
+                principalTable: "ApplicationUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Monsters_ApplicationUser_UserId",
+                table: "Monsters");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationUser");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Monsters_UserId",
+                table: "Monsters");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Monsters");
+        }
+    }
+}
