6c9cce01714476e3d68d52d2df8d3bdab458a2d8
3Emme <emmettbuentiempo@gmail.com>
2020-10-29 12:14:42 -0700

    complete AddIdentityContext migration and get user to pass along userID for monster gets.
    Co-Authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- DungeonApi/Controllers/MonstersController.cs
+++ DungeonApi/Controllers/MonstersController.cs
@@ -46,7 +46,7 @@ namespace DungeonApi.Controllers
     [HttpGet("{id}")]
     public ActionResult<Monster> Get(int id)
     {
-      return _db.Monsters.FirstOrDefault(entry => entry.MonsterId == id);
+      return _db.Monsters.Include(monster => monster.User).FirstOrDefault(entry => entry.MonsterId == id);
     }
 
 
